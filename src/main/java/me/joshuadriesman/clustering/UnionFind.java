9e34a71a2d0d20a3ecdfd813202d88fef57d4d76
Joshua Driesman <jdriesman@live.com>
2015-11-29 18:36:58 -0500

    Added copyright statment to UnionFind

--- src/main/java/me/joshuadriesman/clustering/UnionFind.java
+++ src/main/java/me/joshuadriesman/clustering/UnionFind.java
@@ -2,6 +2,8 @@ package me.joshuadriesman.clustering;
 
 /**
  * Created by Joshua Driesman on 11/29/2015.
+ *
+ * Copyright 2015 Joshua Driesman, All rights reserved
  */
 public class UnionFind {
 }
