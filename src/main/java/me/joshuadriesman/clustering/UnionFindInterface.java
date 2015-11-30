6bf463f42c0fd6e61b02a3878132ed9dd0b9c492
Joshua Driesman <jdriesman@live.com>
2015-11-30 00:44:19 -0500

    Made UnionFindInterface.

--- src/main/java/me/joshuadriesman/clustering/UnionFindInterface.java
+++ src/main/java/me/joshuadriesman/clustering/UnionFindInterface.java
@@ -4,7 +4,16 @@ package me.joshuadriesman.clustering;
  * Created by Joshua Driesman on 11/29/2015.
  */
 public interface UnionFindInterface {
-    public ImageSegment find(ImageSegment i);
+    /**
+     * Find the representative of the set that this ImageSegment is in.
+     * @param i the ImageSegment who's set you want to look up
+     * @return the representative of the set
+     */
+    ImageSegment find(ImageSegment i);
 
-    public void union(Edge e);
+    /**
+     * Union the two sets connected by that edge.
+     * @param e the edge to add to the graph.
+     */
+    void union(Edge e);
 }
