efaea31767dfda2b83dda1d508994a190609a7d0
Joshua Driesman <jdriesman@live.com>
2015-11-30 00:47:46 -0500

    Created UnionFind interface and initial implementation of methods.

--- src/main/java/me/joshuadriesman/clustering/UnionFind.java
+++ src/main/java/me/joshuadriesman/clustering/UnionFind.java
@@ -1,7 +1,9 @@
 package me.joshuadriesman.clustering;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 /**
  * Created by Joshua Driesman on 11/29/2015.
@@ -9,21 +11,27 @@ import java.util.HashMap;
  * Copyright 2015 Joshua Driesman, All rights reserved
  */
 public class UnionFind implements UnionFindInterface{
-    private HashMap<ImageSegment, ImageSegment> representatives;
+    private HashMap<LineData, LineData> representatives;
+    private ArrayList<Edge> edges  = new ArrayList<>();
 
-    public UnionFind(ArrayList<ImageSegment> segments) {
-        for (ImageSegment s : segments) {
-
+    public UnionFind(ArrayList<LineData> segments) {
+        for (LineData s : segments) {
+            edges.addAll(segments.stream().filter(s2 -> s != s2).map(s2 -> new Edge(s, s2)).collect(Collectors.toList()));
         }
+        Collections.sort(edges);
     }
 
     @Override
-    public ImageSegment find(ImageSegment i) {
+    public LineData find(LineData i) {
         return null;
     }
 
     @Override
     public void union(Edge e) {
 
+    }
+
+    public ArrayList<Edge> getEdges() {
+        return edges;
     }
 }
