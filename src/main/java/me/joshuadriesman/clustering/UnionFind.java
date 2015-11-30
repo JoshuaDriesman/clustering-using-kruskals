68a1d03dbed66780b6176477b20c1f0cdf7ea44e
Joshua Driesman <jdriesman@live.com>
2015-11-30 13:37:09 -0500

    Edges are now generated and sorted when the nodes are added to the union find structure. Test was written to make sure edges were sorted properly.

--- src/main/java/me/joshuadriesman/clustering/UnionFind.java
+++ src/main/java/me/joshuadriesman/clustering/UnionFind.java
@@ -3,7 +3,7 @@ package me.joshuadriesman.clustering;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.stream.Collectors;
+import java.util.List;
 
 /**
  * Created by Joshua Driesman on 11/29/2015.
@@ -11,12 +11,17 @@ import java.util.stream.Collectors;
  * Copyright 2015 Joshua Driesman, All rights reserved
  */
 public class UnionFind implements UnionFindInterface{
+    private List<Edge> edges  = new ArrayList<>();
     private HashMap<LineData, LineData> representatives;
-    private ArrayList<Edge> edges  = new ArrayList<>();
 
-    public UnionFind(ArrayList<LineData> segments) {
-        for (LineData s : segments) {
-            edges.addAll(segments.stream().filter(s2 -> s != s2).map(s2 -> new Edge(s, s2)).collect(Collectors.toList()));
+    public UnionFind(List<LineData> segments) {
+        for (int i = 0; i < segments.size() - 1; i++) {
+            LineData nodeA = segments.get(i);
+            for (int i2 = i + 1; i2 < segments.size(); i2++) {
+                LineData nodeB = segments.get(i2);
+
+                edges.add(new Edge(nodeA, nodeB));
+            }
         }
         Collections.sort(edges);
     }
@@ -31,7 +36,7 @@ public class UnionFind implements UnionFindInterface{
 
     }
 
-    public ArrayList<Edge> getEdges() {
+    public List<Edge> getEdges() {
         return edges;
     }
 }
--- src/test/java/me/joshuadriesman/clustering/UnionFindTest.java
+++ src/test/java/me/joshuadriesman/clustering/UnionFindTest.java
@@ -1,7 +1,27 @@
 package me.joshuadriesman.clustering;
 
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by Joshua Driesman on 11/30/2015.
+ *
+ * Copyright 2015 Joshua Driesman, All rights reserved
  */
 public class UnionFindTest {
+    @Test
+    public void testSortingOfEdges() {
+        List<LineData> imageSegments = new ArffParser("src/main/resources/segment-full.arff").parseWhole();
+        UnionFind uf = new UnionFind(imageSegments);
+
+        List<Edge> edges = uf.getEdges();
+
+        // Make sure list of edges is sorted properly
+        for (int i=0; i<edges.size() - 1; i++) {
+            assertTrue(edges.get(i).getWeight() <= edges.get(i + 1).getWeight());
+        }
+    }
 }
