11c3a53dfd54ad187450471fd637dfc9dddd8959
Joshua Driesman <jdriesman@live.com>
2015-12-01 12:42:25 -0500

    Added JavaDoc for writeLine. Moved close method out as it is implemented in the closeable interface.

--- src/main/java/me/joshuadriesman/clustering/IResultWriter.java
+++ src/main/java/me/joshuadriesman/clustering/IResultWriter.java
@@ -6,9 +6,11 @@ package me.joshuadriesman.clustering;
  * Copyright 2015 Joshua Driesman, All rights reserved
  */
 public interface IResultWriter {
+    /**
+     * Writes a line to the file.
+     * @param line the line to write to the file
+     * @throws java.nio.file.ClosedFileSystemException if the writer has already been closed
+     * @throws java.io.IOException if the line could not be written
+     */
     public void writeLine(String line);
-
-    public void close(String line);
-
-    public boolean closed();
 }
