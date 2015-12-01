ad14fc8f5b230154e7cc82eac66d57279cc7a531
Joshua Driesman <jdriesman@live.com>
2015-12-01 12:53:49 -0500

    Initial commit for ResultWriterTest

---
+++ src/test/java/me/joshuadriesman/clustering/ResultWriterTest.java
@@ -0,0 +1,9 @@
+package me.joshuadriesman.clustering;
+
+/**
+ * Created by Joshua Driesman on 12/1/2015.
+ *
+ * Copyright 2015 Joshua Driesman, All rights reserved
+ */
+public class ResultWriterTest {
+}
